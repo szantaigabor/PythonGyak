a0265c1f1f18108959f5b9c443fcd59715288c25
Gabi <szantai.gabor.19f@szlgbp.hu>
2022-01-21 12:53:19 +0100

    3000B

---
+++ 3000B.py
@@ -0,0 +1,15 @@
+""""
+Olvassuk be az alábbi fájl tartalmmát egy listába/tömbbe,
+majd a következő feladatokat oldjuk meg.
+Minden feladat előtt a program írja ki a feladat sorszámát!
+1. Van-e a sorozatban pozitív szám?
+2. Hány eleme van a sorozatnak? 
+3. Mennyi a sorozatban található legkisebb szám?
+4. Írjuk ki az első 33-mal osztható szám indexét!
+5. Mennyi a sorozatban található számok átlagának a fele?
+6. Igaz-e, hogy minden szám pozitív?
+7. Hány páratlan szám található a sorozatban?
+8. Van-e a sorozatban olyan negatív szám, amelyet újabb negatív követ?
+9. Írjuk ki az utolsó 19-cel osztható szám indexét!
+10. Írjuk ki a sorozatban található 5-tel osztható számokat!
+"""
