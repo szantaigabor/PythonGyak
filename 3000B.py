5d65d46dafaa982951327b369d9dc2cf309615bc
Gabi <szantai.gabor.19f@szlgbp.hu>
2022-01-21 13:09:58 +0100

    hnjgftrjhf

--- 3000B.py
+++ 3000B.py
@@ -13,3 +13,6 @@ Minden feladat előtt a program írja ki a feladat sorszámát!
 9. Írjuk ki az utolsó 19-cel osztható szám indexét!
 10. Írjuk ki a sorozatban található 5-tel osztható számokat!
 """
+
+
+jhz
