647fa19d5a1a20577473466677c6f8c85ac78d94
Szantai Gabor <szantai.gabor.19f@szlgbp.hu>
2022-01-21 13:13:47 +0100

    print2

--- 3000B.py
+++ 3000B.py
@@ -13,4 +13,4 @@ Minden feladat előtt a program írja ki a feladat sorszámát!
 9. Írjuk ki az utolsó 19-cel osztható szám indexét!
 10. Írjuk ki a sorozatban található 5-tel osztható számokat!
 """
-
+print('2')
